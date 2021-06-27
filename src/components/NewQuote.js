a0dca3955d954d73018adcd66b5f2e85d25178e5
Omar Alvarez <alvarezomar684@gmail.com>
2021-06-26 17:23:50 -0700

    se retira target blank del button twitter ya que mandaba un warning respecto a esto

--- src/components/NewQuote.js
+++ src/components/NewQuote.js
@@ -7,7 +7,7 @@ const NewQuote = ({changed,quoteOfListT,authorOfListT}) => {
     return(
         <div className="buttons-style">           
             <button onClick={changed}>New Quote</button>
-            <a className="twitter-share-button" target="_blank"
+            <a className="twitter-share-button"
                 href={`https://twitter.com/intent/tweet?text= "${quoteOfListT}" 
 
                 -${authorOfListT}`}  >
